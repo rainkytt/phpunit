4d333dbe4dce6abbc90f69d035a1a428cb530e5c
Rain <rain.kutt@khk.ee>
2019-01-18 16:14:02 +0200

    01 Write a unit test 01_06

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -3,15 +3,15 @@ namespace TDD\Test;
 require 'vendor\autoload.php';
 
 use PHPUnit\Framework\TestCase;
-use TDD\Receipt;
+use TDD\Receipt; // alusfail kasutamiseks
 
 class ReceiptTest extends TestCase {
 	public function testTotal() {
-		$Receipt = new Receipt();
-		$this->assertEquals(
-			15,
-			$Receipt->total([0,2,5,8]),
-			'When summing the total should equal 15'
+		$Receipt = new Receipt(); // klassist teeb objekti
+		$this->assertEquals( // viitab TestCase-le
+			15, // oodatud väärtus
+			$Receipt->total([0,2,5,8]), // Receipt klassist pärit objekt, mis saab massiivi
+			'When summing the total should equal 15' // kuvatakse kui ei tule oodatud väärtust
 		);
 	}
 }
